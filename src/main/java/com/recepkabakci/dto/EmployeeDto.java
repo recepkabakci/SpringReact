e64a350a19f594c633f836700eeea12289ae591a
RECEP KABAKCI <recepkabakci90@gmail.com>
2023-05-01 15:29:48 +0200

    Spring React

---
+++ src/main/java/com/recepkabakci/dto/EmployeeDto.java
@@ -0,0 +1,21 @@
+package com.recepkabakci.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
+public class EmployeeDto {
+    private Long employeeId;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+
+
+}
